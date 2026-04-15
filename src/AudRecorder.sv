c626b0c868a2acfa79af91fc611c6af47ea40aba
PinShiang <b12901095@ntu.edu.tw>
2026-04-15 17:37:37 +0800

    addfile2

---
+++ src/AudRecorder.sv
@@ -0,0 +1,13 @@
+module AudRecorder(
+    input        i_rst_n,
+    input        i_clk,
+    input        i_lrc,
+    input        i_start,
+    input        i_pause,
+    input        i_stop,
+    input        i_data,
+    output       o_address,
+    output       o_data
+);
+
+endmodule
