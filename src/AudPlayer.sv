f4e673cab2766f95d6c2399ac5140be268e8ae72
PinShiang <b12901095@ntu.edu.tw>
2026-04-15 17:37:37 +0800

    addfile2

--- src/AudPlayer.sv
+++ src/AudPlayer.sv
@@ -1,10 +1,10 @@
 module AudPlayer(
-	.i_rst_n(i_rst_n),
-	.i_bclk(i_AUD_BCLK),
-	.i_daclrck(i_AUD_DACLRCK),
-	.i_en(), // enable AudPlayer only when playing audio, work with AudDSP
-	.i_dac_data(dac_data), //dac_data
-	.o_aud_dacdat(o_AUD_DACDAT)
+	input i_rst_n,
+	input i_bclk,
+	input i_daclrck,
+	input i_en, // enable AudPlayer only when playing audio
+	input i_dac_data, //dac_data passed by audDSP
+	output o_aud_dacdat // pass to wm8731
 );
 
 endmodule
---
+++ src/I2CInit.sv
@@ -0,0 +1,11 @@
+module I2cInitializer(
+    input        i_rst_n,
+    input        i_clk,
+    input        i_start,
+    output       o_finished,
+    output       o_sclk,
+    inout        o_sdat,
+    output       o_oen
+);
+
+endmodule
