a8878ed9c2fcf287ea8144c3d1a92c63d0acfee7
William <williamsu0011@gmail.com>
2026-04-27 17:16:54 +0800

    update FSM in I2C

--- src/I2C.sv
+++ src/I2C.sv
@@ -190,7 +190,7 @@ module I2cInitializer (
                         SCL_w = 0;
                     end
                     7: begin
-                        state_w = data_word_idx_r == 9 ? STOP : BUFFER;
+                        state_w = data_word_idx_r == 9 ? STOP : DATA;
                     end
                     default: begin
                         SDA_w = SDA_r;
